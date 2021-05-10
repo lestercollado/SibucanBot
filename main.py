d2fbfd942afe9f79c921c40857dae42739682a02
Lester Collado Rolo <lcollado@uci.cu>
2021-05-10 19:16:24 -0400

    Add db to configuration. Fix others.

--- main.py
+++ main.py
@@ -2,50 +2,50 @@ import os
 import pyshorteners
 import json
 import requests
+import sqlite3
+from sqlite3 import Error
 from uuid import uuid4
 from telegram.ext import Updater, CommandHandler, ConversationHandler, Filters, CallbackQueryHandler, InlineQueryHandler, ChosenInlineResultHandler, MessageHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ChatAction, ParseMode, InlineQueryResultArticle, InputTextMessageContent
 
-# Stages
+#Stages
 INPUT_TEXT = 0
 FIRST = 1
 
-municipality_send = 0
+#URL services
 url_base = 'https://sibucan-frontend-staging.herokuapp.com/services/'
+
+#Paginate
 offset = 1
 
-#Get the cities from json file
+#Get the cities
 def getCities():
-    with open("json/cities.json") as jsonFile:
-        jsonObject = json.load(jsonFile)
-        jsonFile.close()   
+    response = requests.get('http://sibucan-backend-staging.herokuapp.com/cities/')
+    jsonObject = response.json()
 
     cities = {}
 
     for city in jsonObject:
         cities[city['name']]=city['id']
 
+    print('Devolviendo provincias')
     return cities
 
-#Get the municipalities from json file
+#Get the municipalities
 def getMunicipalities():   
-    with open("json/municipalities.json") as jsonFile:
-        jsonObject = json.load(jsonFile)
-        jsonFile.close()    
-
     municipalities = {}
 
     for id in cities.values():
         municipalities[id]=getMunicipalitiesByCity(id)
 
+    print('Devolviendo municipios')
     return municipalities
 
-#Get the municipalities from json file with city
-def getMunicipalitiesByCity(city):   
-    with open("json/municipalities.json") as jsonFile:
-        jsonObject = json.load(jsonFile)
-        jsonFile.close()
-    
+#Get the municipalities with city
+def getMunicipalitiesByCity(city):  
+    response = requests.get('http://sibucan-backend-staging.herokuapp.com/municipalities/')
+    jsonObject = response.json()
+
     municipalities = {}
 
     for municipality in jsonObject:
@@ -54,26 +54,35 @@ def getMunicipalitiesByCity(city):
 
     return municipalities
 
+#Start bot
 def start(update, context):
+    values = (int(update.message.chat['id']),0)
+    sql_insert(values)
     update.message.reply_text(
         text = "👋 Hola, soy un bot para realizar búsquedas en la plataforma Sibucan.\n ⁉️ Consulta la /ayuda para que aprenda como utilizarme"
     )
 
+#Help
 def help(update, context):
     update.message.reply_text(
         text = '''⁉️ ¿Cómo buscar? 👇
-@sibucan_bot <em><u>texto</u></em> Buscar en Sibucan, sustituya <em><u>texto</u></em> por el término que desea buscar
+        
+📍 Use /filtro_provincia_municipio para buscar en un el municipio que desee
+❌ Para buscar sin filtro de municipio, utilice /borrar_filtro
+✏️ Escriba @sibucan_bot <em><u>texto</u></em>
+🔎 Sustituya <em><u>texto</u></em> por el término que desea buscar
 
 <b>Comandos: </b>
-/elegir_provincia_municipio Elegir provincia y municipio para filtrar las búsquedas
-/borrar_municipio Elimina el filtro de municipio para las búsquedas
+/filtro_provincia_municipio Elegir provincia y municipio para filtrar las búsquedas
+/borrar_filtro Elimina el filtro de municipio para las búsquedas
 /nosotros Información sobre la plataforma Sibucan
 /emprendedores Información para los emprendedores
 /clientes Información para los clientes''',
         parse_mode=ParseMode.HTML
     )
 
-def about(update, context):
+#About, us.
+def about(update, context): 
     update.message.reply_text(
         text = '''<b>📌 Sobre nosotros</b>
 <b>SIBUCAN</b> es un proyecto que busca estimular la iniciativa de los emprendedores cubanos y promover la calidad de los servicios y productos recibidos por los clientes.
@@ -81,6 +90,7 @@ A través de nuestra plataforma, los usuarios pueden ofrecer y recibir servicios
         parse_mode=ParseMode.HTML
     )
 
+#Entrepreneurs info
 def entrepreneurs(update, context):
     update.message.reply_text(
         text = '''<b>📌 Para Emprendedores</b>
@@ -89,6 +99,7 @@ Para hacerlo aún más atractivo, se puede acceder a nuestra plataforma desde cu
         parse_mode=ParseMode.HTML
     )
 
+#Customers info
 def customers(update, context):
     update.message.reply_text(
         text = '''<b>📌 Para Clientes</b>
@@ -97,6 +108,7 @@ SIBUCAN lo pone en contacto directo con miles de emprendedores locales y le perm
         parse_mode=ParseMode.HTML
     )
 
+#Municipalities Buttons
 def keyboard_municipalities(update,context):    
     query = update.callback_query
     city = int(query.data.split()[1])
@@ -126,6 +138,7 @@ def keyboard_municipalities(update,context):
 
     return FIRST
 
+#Provinces Buttons
 def search_command_handler(update,context):    
     keyboard_cities = []
 
@@ -147,6 +160,7 @@ def search_command_handler(update,context):
     update.message.reply_text('<b>📍 Selecciona una provincia:</b>', reply_markup=reply_markup,parse_mode = ParseMode.HTML)
     return FIRST
 
+#Provinces Buttons back
 def search_command_handler_back(update,context):    
     query = update.callback_query
     keyboard_cities = []
@@ -171,38 +185,44 @@ def search_command_handler_back(update,context):
     query.edit_message_text(text=f"<b>📍 Selecciona una provincia</b>", reply_markup=reply_markup, parse_mode = ParseMode.HTML)
     return FIRST
 
+#Command erase municipality filter
 def erase_municipality_handler(update,context):   
-    global municipality_send
-    municipality_send = 0
+    chat_id = update.message.chat['id']
+    values = (0,chat_id)
+    sql_update(values)
     update.message.reply_text("Ahora puede comenzar a utilizar el comando @sibucan_bot para buscar.",parse_mode = ParseMode.HTML)
 
+#Action erase municipality filter from button
 def erase_municipality(update,context):   
-    global municipality_send
-    municipality_send = 0
     query = update.callback_query
+    chat_id = query['message']['chat']['id']
+    values = (0,chat_id)
+    sql_update(values)   
     query.answer()
     query.edit_message_text(text="Ahora puede comenzar a utilizar el comando @sibucan_bot para buscar.", parse_mode = ParseMode.HTML)
     return FIRST
 
+#Ready to search with municipality
 def search_services(update,context):
     query = update.callback_query    
-    global municipality_send
-    municipality_send = int(query.data.split()[1])
+    chat_id = query['message']['chat']['id']
+    values = (int(query.data.split()[1]),chat_id)
+    sql_update(values)
     query.answer()
-    query.edit_message_text(text="Ahora puede comenzar a utilizar el comando @sibucan_bot para buscar en el municipio elegido.")
+    query.edit_message_text(text='''Ahora puede comenzar a utilizar el comando @sibucan_bot para buscar en el municipio elegido.\nSi desea quitar la selección usa /borrar_filtro''')
     return FIRST
 
+#Search
 def inlinequery(update, context):
     global offset
 
     query = update.inline_query.query
-    
+
+    chat_id = (update.inline_query.from_user.id,)
+
+    municipality_id = sql_select(chat_id)
+
     results = []
-    
-    if municipality_send == 0:
-        params = {'search': query, 'sort': 'top-rated', 'page_size': 10, 'page': offset}
-    else:        
-        params = {'municipality':municipality_send, 'search': query, 'sort': 'top-rated', 'page_size': 10, 'page': offset}
 
     if query == "":
         offset = 1
@@ -213,71 +233,107 @@ def inlinequery(update, context):
                     input_message_content=InputTextMessageContent("Debe escribir un texto para buscar en Sibucan.\nPor ejemplo: @sibucan_bot carpintero")
                 )
         )
-        update.inline_query.answer(results)
+        update.inline_query.answer(results,cache_time=1)
+        return FIRST
+    else: 
+        text = query                   
+
+        if municipality_id == 0:
+            params = {'search': text, 'sort': 'top-rated', 'page_size': 10, 'page': offset}
+        else:        
+            params = {'municipality':municipality_id, 'search': text, 'sort': 'top-rated', 'page_size': 10, 'page': offset}
+
+        response = requests.get('http://sibucan-backend-staging.herokuapp.com/services/', params=params)
+        
+        if response.status_code == 200:
+            print("Mun",municipality_id,"Resultados",response.json()['count'])               
+            if response.json()['count'] != 0:
+                services = response.json()['results']
+                for service in services:
+                    url_service = url_base+str(service["id"])+"/"
+                    #Code line to use url short
+                    #url_short = short_url(url_base+str(service["id"])+"/")
+                    status_service = "🕐 Abierto" if service["open_now"] == True else "🕐 Cerrado"
+                    telephone = "☎️ " + service["telephone"] if service["telephone"] != "" else "☎️ "+"No" 
+                    cellphone = "📱 " + service["cellphone"] if service["cellphone"] != "" else "📱 "+"No"
+                    description = '⭐️ '+str(service["average_rating"])+ "\n" + telephone + cellphone
+                    results.append(
+                        InlineQueryResultArticle(
+                            id=str(uuid4()),
+                            title= "🔰 "+service["name"],
+                            thumb_url = service["logo"],
+                            url = url_service,
+                            hide_url = True,
+                            description = description,
+                            input_message_content=InputTextMessageContent("🔰 "+"<b>"+service["name"]+"</b>\n"+telephone+'\n'+cellphone+"\n"+status_service+"\n"+"⭐️ "+str(service["average_rating"])+"\n"+"🌐 "+url_service,parse_mode=ParseMode.HTML),
+                        )
+                    )                         
+                offset = offset + 1
+                update.inline_query.offset=offset
+                update.inline_query.answer(results,next_offset=offset,cache_time=1)
+            else:
+                results.append(
+                        InlineQueryResultArticle(
+                            id=str(uuid4()),
+                            title="No se han encontrado resultados.",
+                            description = "Más detalles",
+                            input_message_content=InputTextMessageContent("Intenta cambiar de municipio o modificar el término de su búsqueda.")
+                        )
+                )
+                update.inline_query.answer(results,next_offset=None,cache_time=1)
+        else:
+            offset = 1
+            results = []
+            update.inline_query.answer(results,next_offset=None,cache_time=1)
+            return FIRST   
+
         return FIRST
 
-    response = requests.get('http://sibucan-backend-staging.herokuapp.com/services/', params=params)
-    
-    if response.status_code == 200:
-        if response.json()['count'] != 0:
-            services = response.json()['results']
-            for service in services:
-                url_service = url_base+str(service["id"])+"/"
-                # url_short = short_url(url_base+str(service["id"])+"/")
-                if service["open_now"] == True:
-                    results.append(
-                        InlineQueryResultArticle(
-                            id=str(uuid4()),
-                            title=service["name"],
-                            thumb_url = service["logo"],
-                            url = url_service,
-                            hide_url = True,
-                            description = "Teléfonos: " + service["telephone"] + ' ' + service["telephone"]+'\n'+"Ver más",
-                            input_message_content=InputTextMessageContent("<b>"+service["name"]+"</b>\n"+"☎️Teléfonos: " + service["telephone"] + ' ' + service["telephone"]+"\n"+"🕐 Abierto: Sí"+"\n"+"⭐️ "+str(service["average_rating"])+"\n"+"🌐 "+url_service,parse_mode=ParseMode.HTML),
-                        )
-                    )    
-                else: 
-                    results.append(
-                        InlineQueryResultArticle(
-                            id=str(uuid4()),
-                            title=service["name"],
-                            thumb_url = service["logo"],
-                            url = url_service,
-                            hide_url = True,
-                            offset = 4,
-                            description = "Teléfonos: " + service["telephone"] + ' ' + service["telephone"]+'\n'+"Ver más",
-                            input_message_content=InputTextMessageContent("<b>"+service["name"]+"</b>\n"+"☎️ Teléfonos: " + service["telephone"] + ' ' + service["telephone"]+"\n"+"🕐 Abierto: No"+"\n"+"⭐️ "+str(service["average_rating"])+"\n"+"🌐 "+url_service,parse_mode=ParseMode.HTML),
-                        )
-                    )    
-            offset = offset + 1
-            update.inline_query.offset=offset
-            update.inline_query.answer(results,next_offset=offset)
-        else:
-            results.append(
-                    InlineQueryResultArticle(
-                        id=str(uuid4()),
-                        title="No se han encontrado resultados.",
-                        description = "Más detalles",
-                        input_message_content=InputTextMessageContent("Intenta cambiar de municipio o modificar el término de su búsqueda.")
-                    )
-            )
-            update.inline_query.answer(results,next_offset=None)
-    else:
-        offset = 1
-        results = []
-        update.inline_query.answer(results,next_offset=None)
-        return FIRST   
-    
-    return FIRST
-
+#Short URL
 def short_url(url):
     s = pyshorteners.Shortener()
     short_u = s.chilpit.short(url)
     return short_u
 
+#Create Table
+def create_table():    
+    cursorObj.execute("CREATE TABLE IF NOT EXISTS data(id integer PRIMARY KEY, chat_id integer, municipality integer)")
+    con.commit()
+
+#Connection with database
+def sql_connection():    
+    try:
+        con = sqlite3.connect('config.db',check_same_thread=False)
+        print("Connection is established.")
+        return con
+    except Error:
+        print(Error)
+
+#Insert query
+def sql_insert(values):    
+    cursorObj.execute('INSERT INTO data(chat_id, municipality) VALUES(?, ?)', values)    
+    con.commit()
+
+#Update query
+def sql_update(values): 
+    cursorObj.execute('UPDATE data SET municipality = ? where chat_id = ?', values)    
+    con.commit()
+
+#Select query
+def sql_select(value):
+    cursorObj.execute('SELECT municipality FROM data where chat_id = ?',value)
+    municipality = cursorObj.fetchall()[0][0]
+    return municipality
+
+#Main
 if __name__ == '__main__':    
     cities = getCities()
     municipalities = getMunicipalities()
+
+    con = sql_connection()
+    cursorObj = con.cursor()    
+    create_table()
+
     updater = Updater(token='1775317884:AAEXOrL96uPN5mW-ECgrYjMR3yMAUXllHtk', use_context=True)
 
     dp = updater.dispatcher
@@ -291,12 +347,12 @@ if __name__ == '__main__':
     dp.add_handler(InlineQueryHandler(inlinequery))  
     dp.add_handler(ConversationHandler(
         entry_points = [
-            CommandHandler('elegir_provincia_municipio', search_command_handler),
-            CommandHandler('borrar_municipio', erase_municipality_handler),            
+            CommandHandler('filtro_provincia_municipio', search_command_handler),
+            CommandHandler('borrar_filtro', erase_municipality_handler),            
         ],
         states = {
             FIRST: [ 
-                CommandHandler('elegir_provincia_municipio', search_command_handler),
+                CommandHandler('filtro_provincia_municipio', search_command_handler),
                 CallbackQueryHandler(erase_municipality, pattern='erase_municipality'),
                 CallbackQueryHandler(search_services, pattern='search '+'*'),
                 CallbackQueryHandler(keyboard_municipalities, pattern='keyboard_municipalities '+'*'),
